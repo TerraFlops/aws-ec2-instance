7801bce55c4d228c842d61c70a4a16ded2a49bdc
Damian Sloane <damian@capacitorlabs.com>
2020-09-01 10:49:23 +1000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Create EC2 instances
+resource "aws_instance" "instance" {
+  ami = var.ami_id
+  monitoring = var.monitoring
+  instance_type = var.instance_type
+  key_name = var.key_name
+  get_password_data = var.get_password_data
+  vpc_security_group_ids = var.security_group_ids
+  subnet_id = var.subnet_id
+  private_ip = var.private_ip
+  iam_instance_profile = var.iam_instance_profile
+  tags = {
+    Name = var.name
+    Description = var.description
+  }
+  lifecycle {
+    ignore_changes = [
+      key_name
+    ]
+  }
+}
+
+# Create elastic IPs where required
+resource "aws_eip" "server_eip" {
+  count = var.elastic_ip == true ? 1 : 0
+  vpc = true
+  tags = {
+    Name = var.name
+  }
+}
+
+# Associate elastic IPs with their instances
+resource "aws_eip_association" "server_eip_association" {
+  count = var.elastic_ip == true ? 1 : 0
+  public_ip = aws_eip.server_eip[0].public_ip
+  instance_id = aws_instance.instance.id
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "name" {
+  description = "Name of the instance"
+  type = string
+}
+
+variable "description" {
+  description = "Description of the instance"
+  type = string
+}
+variable "ami_id" {
+  description = "AMI image ID from which the instance will be created"
+  type = string
+}
+
+variable "private_ip" {
+  description = "Optional static private IP address to allocate to the instance. If not specified it will be given by DHCP"
+  type = string
+  default = null
+}
+
+variable "subnet_id" {
+  description = "The AWS subnet ID into which the instance will be launched"
+  type = string
+}
+
+variable "security_group_ids" {
+  description = "Set of AWS security group IDs that will be attached to the instance"
+  type = set(string)
+}
+
+variable "instance_type" {
+  description = "EC2 instance type to launch. Defaults to t3a.nano"
+  type = string
+  default = "t3a.nano"
+}
+
+variable "key_name" {
+  description = "Optional EC2 key pair to associate with the instance"
+  type = string
+  default = null
+}
+
+variable "elastic_ip" {
+  description = "Boolean flag, if true an Elastic IP will be allocated to the instance"
+  type = bool
+  default = false
+}
+
+variable "iam_instance_profile" {
+  description = "Optional IAM instance profile name"
+  type = string
+  default = null
+}
+
+variable "monitoring" {
+  description = "Enable detailed instance monitoring"
+  type = bool
+  default = true
+}
+
+variable "get_password_data" {
+  description = "Boolean flag, if true when provisioning the instance the initial password will be retrieved from the instance. This cannot be changes to true after initial deployment as the password is no longer retrievable"
+  type = bool
+  default = false
+}
