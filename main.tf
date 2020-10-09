2cb71c29a3f3a24ba4ce2d80aa6edd1a6253058f
Damian Sloane <damian@capacitorlabs.com.au>
2020-10-09 23:41:58 +1100

    WIP

--- main.tf
+++ main.tf
@@ -27,6 +27,17 @@ resource "aws_instance" "instance" {
   disable_api_termination = var.termination_protection
 }
 
+resource "aws_volume_attachment" "volume" {
+  device_name = "/dev/nvme0n1"
+  volume_id = aws_ebs_volume.volume.id
+  instance_id = aws_instance.instance.id
+}
+
+resource "aws_ebs_volume" "volume" {
+  availability_zone = aws_instance.instance.availability_zone
+  size = var.volume_size
+}
+
 # Create elastic IPs where required
 resource "aws_eip" "server_eip" {
   count = var.elastic_ip == true ? 1 : 0
--- variables.tf
+++ variables.tf
@@ -24,6 +24,12 @@ variable "user_data" {
   default = ""
 }
 
+variable "volume_size" {
+  description = "Volume size"
+  type = number
+  default = 8
+}
+
 variable "subnet_id" {
   description = "The AWS subnet ID into which the instance will be launched"
   type = string
